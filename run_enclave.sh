97d866ae185cfa5e208a3a0244af5994d94d0a65
Louis Zhang <louisz@fb.com>
2022-06-14 10:51:30 -0700

    Terminate enclaves

--- run_enclave.sh
+++ run_enclave.sh
@@ -5,7 +5,11 @@ docker build -f container/Dockerfile -t enclave .
 
 # Convert image to EIF
 nitro-cli build-enclave --docker-uri enclave --output-file enclave.eif
-nitro-cli run-enclave --eif-path enclave.eif --memory 8192 --cpu-count 2 --debug-mode
+
+# Can only have one enclave
+nitro-cli terminate-enclave --all
+
+nitro-cli run-enclave --eif-path enclave.eif --memory 12288 --cpu-count 2 --debug-mode
 
 ENCLAVE_ID=$(nitro-cli describe-enclaves | jq -r .[0].EnclaveID)
 nitro-cli console --enclave-id "$ENCLAVE_ID"
